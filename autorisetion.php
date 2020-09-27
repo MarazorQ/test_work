defa69bb15d2c94a16c6ab7a6f5bc8e834a3a285
Artem Shkuratov <shuratov2001@mail.ru>
2020-09-27 22:19:54 +0300

    release v3

--- autorisetion.php
+++ autorisetion.php
@@ -1,3 +1,7 @@
+<?php
+	session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,20 +10,28 @@
 	<link rel="stylesheet" href="index.css">
 </head>
 <body>
-	<form action="" class="profile">
+	<form action="log_in.php" method="post" class="profile">
 		<h1> Log in</h1>
 		<div class="item">
 			<label for=""> login: </label>
-			<input type="text" placeholder="input your login">
+			<input type="text" name="login" placeholder="input your login">
 		</div>
 		<div class="item"> 
 			<label for=""> password: </label>
-			<input type="password" placeholder="input your password">
+			<input type="password" name="password" placeholder="input your password">
 		</div>
 		<div class="item">
-			<button>LOG IN</button>
+			<button type="submit">LOG IN</button>
 		</div>
 		<p>Don't have an account?-<a href="register.php">registretion</a></p>
+
+			<?php 
+					if ($_SESSION['error']){
+						echo '<p class="error">' . $_SESSION['error'] . '</p>' ;
+					}
+					
+  					unset($_SESSION['error']);	
+				?>
 	</form>
 </body>
 </html>
---
+++ log_in.php
@@ -0,0 +1,10 @@
+<?php 
+	
+	session_start();
+
+	$login = $_POST['login'];
+	$password = $_POST['password'];
+
+	
+
+?>
